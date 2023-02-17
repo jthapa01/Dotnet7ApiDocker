6ccf8603db2a57f31ec0a107460f0d39c379e273
jeevan thapa <jeevanthapa@microsoft.com>
2023-02-16 22:56:23 -0500

    Dotnet 7 API dockerization

---
+++ CompanyEmployees/Migrations/20221117114113_AddedRolesToDb.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace CompanyEmployees.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddedRolesToDb : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "AspNetRoles",
+                columns: new[] { "Id", "ConcurrencyStamp", "Name", "NormalizedName" },
+                values: new object[,]
+                {
+                    { "874f27bf-b15b-410d-9a1b-adf0f360ace4", null, "Administrator", "ADMINISTRATOR" },
+                    { "cb56c861-57e6-4d16-9109-9e206b9ea227", null, "Manager", "MANAGER" }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "874f27bf-b15b-410d-9a1b-adf0f360ace4");
+
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "cb56c861-57e6-4d16-9109-9e206b9ea227");
+        }
+    }
+}
